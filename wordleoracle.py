558d6b4133750208db278d99badcf11c9c987eb7
evansutcliffe <evansutcliffe@Gmail.com>
2022-02-16 15:17:20 +0000

    Add files via upload
    fixed bug in implementation of rules for wordle game, exposed another bug into how word list pruning is performed

--- wordleoracle.py
+++ wordleoracle.py
@@ -1,12 +1,12 @@
 import numpy as np
 import random 
 
-class WordleOracle (): # inherits from abstract class Shape
+class WordleOracle ():
         def __init__ (self):
             self.secret_word = ""
             self.playing = False
             self.turn = 0
-            self.dict = get_possible_words()
+            self.dictionary = get_possible_words()
             self.results_list = []
             
         def play (self):
@@ -20,23 +20,38 @@ class WordleOracle (): # inherits from abstract class Shape
         def set_word(self,word):
             self.secret_word = word
             
-        def test_guess(self,guess): # TODO / BUG, shouldn't return yellow if slot is already filled by green
-            self.turn +=1
-            secret_word = self.secret_word
-            if not isinstance(guess, str):
+        def test_guess(self,guess,testing=False,print_vals=False):
+            
+            if not isinstance(guess, str): ## check inputs
                 raise ValueError(guess,"not a string")
-            if not len(guess)==5:
-                raise ValueError(guess,"not 5 letter word")
-            results = np.zeros((5))
             guess = guess.lower()
+            if guess not in self.dictionary and not testing:# (disabled for testing)
+                raise ValueError(guess,"not a valid 5 letter word")
+            results = np.zeros((5),dtype=int)
+            secret_word = self.secret_word
+
             for i in range(5): #  not optimised for speed
                 if guess[i] == secret_word[i]:
                     results[i]=2
-                elif guess[i] in secret_word: # TODO / BUG, shouldn't return yellow if slot is already filled by green
-                    results[i] = 1
-        #         else: 
-        #           results[i] = 0
+            for i in range(5):
+                is_green = results[i]==2
+                is_yellow =  guess[i] in secret_word and not is_green # potentially is yellow
+                letter_green_count = 0
+                for j in range(5):
+                    if guess[j] == guess[i] and results[j]==2:
+                        letter_green_count+=1
+                letter_yellow_count = 0
+                for j in range(i):
+                    if guess[j] == guess[i] and results[j]==1:
+                        letter_yellow_count+=1
+                more_yellow_required = secret_word.count(guess[i]) -  (letter_green_count + letter_yellow_count) >=1
+                
+                if is_yellow and more_yellow_required:
+                    results[i]=1
+                if print_vals:
+                    print(guess[i], "for guess", guess, "in", secret_word," is ",colourmap[results[i]])
             self.results_list.append(results)
+            self.turn +=1
             return results
 
         
